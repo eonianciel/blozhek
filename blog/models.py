d3d78350e23f8e7eae2396f763d891865ff798c6
eien <evangelinadantes@gmail.com>
2020-05-02 17:53:14 +0300

    +comments, auth

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.utils import timezone
 
 
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
@@ -17,3 +16,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=250)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
